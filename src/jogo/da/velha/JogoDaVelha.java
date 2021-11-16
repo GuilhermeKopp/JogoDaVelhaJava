e178ba38e7255875194b11a28415860aed1c9ecd
GuilhermeKopp <guikopp19@gmail.com>
2021-11-16 16:42:41 -0300

    Iniciando

---
+++ src/jogo/da/velha/JogoDaVelha.java
@@ -0,0 +1,22 @@
+package jogo.da.velha;
+
+import javax.swing.*;
+
+public class JogoDaVelha extends JFrame {
+	
+	public JogoDaVelha () {
+		
+		setVisible(true);
+		setTitle("Jogo da Velha");
+		setDefaultCloseOperation(3);
+		setLayout(null);
+		setBounds(0,0,700,500);
+		
+	}
+	public static void main(String[] args) {
+		
+		
+		
+	}
+
+}
